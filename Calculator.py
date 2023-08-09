a8f3e3999feecf5cf7c383aeb4c3727a455268bc
Ayush Soni <aayushsoni5130@gmail.com>
2023-08-09 14:02:07 -0400

    /assign_reviewer @hammerjuggler

--- Calculator.py
+++ Calculator.py
@@ -5,9 +5,14 @@ while True: # for loop
       "2. Subtract\n" \
       "3. Division\n" \
       "4. Multiplication\n" \
+      "5. Square\n" \
+      "6. Square root\n" \
       "0. Exit\n")  #assigned different operation as a numbers
 
     Option = int(input("select operation- 1, 2, 3, 4, 0 :")) #user will be asked to apply which option + - * /
+
+    if Option >= 7:
+       print("inter from number 0, 1, 2, 3, 4 only")
 
     if Option == 0: #0 will exit the cide from loop
       break
@@ -25,10 +30,24 @@ while True: # for loop
 
 
     if Option == 3:   #if user select option 3
-      sum = int(number_1) / int(number_2) # math part 
-      print("Total is= ", sum)  #Output
+      #sum = int(number_1) / int(number_2) # math part 
+      #print("Total is= ", sum)  #Output
+        if number_2 == 0:
+          print("N/A")
+        else:
+          sum = int(number_1) / int(number_2) # math part 
+          print("Total is= ", sum)  #Output
 
     if Option == 4: #if user select option 4
       sum = int(number_1) * int(number_2) # math part 
       print("Total is= ", sum)  #Output
 
+    if Option == 5:
+       sum = int(number_1) * int(number_1)
+       print("will only pick number 1 not number2")
+       print("Total is= ", sum)
+       
+    if Option == 6:
+       sum = int(number_1) ** 0.5
+       print("will only pick number 1 not number2")
+       print("Total is= ", sum)
